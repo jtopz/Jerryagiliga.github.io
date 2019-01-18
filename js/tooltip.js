e442ca9f5b784826691a8b165226e70743d7f0b6
Codeixo (Jerry Agiliga) <jkrativconcepts@gmail.com>
2019-01-17 22:03:01 -0500

    Add files via upload

---
+++ js/tooltip.js
@@ -0,0 +1,8 @@
+$(function () {
+$('.material-tooltip-email').tooltip({
+template: '<div class="tooltip md-tooltip-email">
+  <div class="tooltip-arrow md-arrow"></div>
+  <div class="tooltip-inner md-inner-email"></div>
+</div>'
+});
+})
